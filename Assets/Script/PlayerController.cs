0cae31b668c2afd7991ad0946cf9d63dd41febda
sy lee <sylee@hims.co.kr>
2022-07-27 13:49:13 +0900

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  private Rigidbody m_rigidbody;
+  public float m_fMoveSpeed;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+}
