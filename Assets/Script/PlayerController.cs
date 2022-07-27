7dbd8e8b9e0ad61144b4097671a914fb98241663
sy lee <sylee@hims.co.kr>
2022-07-27 19:31:50 +0900

    Now, your player can move.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,11 +9,16 @@ public class PlayerController : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-
+    m_rigidbody = GetComponent<Rigidbody>();
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (m_rigidbody != null)
+    {       
+      Vector3 v3NewVelocity = new Vector3(Input.GetAxisRaw("Horizontal") * m_fMoveSpeed, 0, Input.GetAxisRaw("Vertical") * m_fMoveSpeed);
+      m_rigidbody.velocity = v3NewVelocity;
+    }
   }
 }
